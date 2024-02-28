ab532e06175a46189423ba59ce739b0fc5a8c2f3
BudiPrasetio17 <budi.tio17@gmail.com>
2024-02-28 10:06:12 +0700

    first commit

---
+++ app/Http/Controllers/authController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class authController extends Controller
+{
+    function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    function callback()
+    {
+        $user = Socialite::driver('google')->user();
+        $id = $user->id;
+        $email = $user->email;
+        //dd($id, $email);
+        $name = $user->name;
+
+        return "$id - $email - $name";
+    }
+}
