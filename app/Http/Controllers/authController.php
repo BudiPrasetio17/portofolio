4758748a1f61f726e2a081732de00e39a8f55d9e
BudiPrasetio17 <budi.tio17@gmail.com>
2024-02-28 17:38:05 +0700

    update v1

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Laravel\Socialite\Facades\Socialite;
 
 class authController extends Controller
 {
+    function index()
+    {
+        return view('auth.index');
+    }
+    
     function redirect()
     {
         return Socialite::driver('google')->redirect();
@@ -19,7 +27,35 @@ class authController extends Controller
         $email = $user->email;
         //dd($id, $email);
         $name = $user->name;
+        $avatar = $user->avatar;
 
-        return "$id - $email - $name";
+        //return "$id - $email - $name";
+        $cek = User::where('email', $email)->count();
+        if($cek > 0) {
+            $avatar_file = $id.'.jpg';
+            $fileContent = file_get_contents($avatar);
+            File::put(public_path('admin/images/faces/'.$avatar_file), $fileContent);
+            $user = User::updateOrCreate(
+                ['email' => $email],
+                [
+                    'name' => $name,
+                    'google_id' => $id,
+                    'avatar' => $avatar_file
+                ]
+            );
+
+            Auth::login($user);
+            return redirect()->to('dashboard')->with('success', 'login berhasil');
+        }else{
+            return redirect()->to('auth')->with('error', 'email tidak terdaftar');
+        }
+
+        
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->to('auth');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\authController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
 use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\halamanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +21,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/auth/redirect', [authController::class, 'redirect']);
+Route::redirect('/home', '/dashboard');
 
-Route::get('/auth/callback', [authController::class, 'callback']);
+Route::get('/auth', [authController::class, 'index'])->name('login')->middleware('guest');
+Route::get('/auth/redirect', [authController::class, 'redirect'])->middleware('guest');
+Route::get('/auth/callback', [authController::class, 'callback'])->middleware('guest');
+Route::get('/auth/logout', [authController::class, 'logout']);
+
+Route::prefix('dashboard')->middleware('auth')->group(
+    function () {
+        Route::get('/', function(){
+            return view('dashboard.layout');
+        });
+        Route::resource('halaman', halamanController::class);
+    }
+);
